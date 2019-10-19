a9d79a901d92ae1b0ba33a7566c998a446b3e0a7
jsjxsy <jsjxsy@gmail.com>
2019-10-19 21:52:39 +0800

    Initial commit

---
+++ app/src/main/obj/local/armeabi-v7a/objs/Threads/com_apress_threads_MainActivity.o.d
@@ -0,0 +1,5 @@
+/Users/admin/Android_NDK/Chapter_7_Source_Code/app/src/main/obj/local/armeabi-v7a/objs/Threads/com_apress_threads_MainActivity.o: \
+  /Users/admin/Android_NDK/Chapter_7_Source_Code/app/src/main/jni/com_apress_threads_MainActivity.cpp \
+  /Users/admin/Android_NDK/Chapter_7_Source_Code/app/src/main/jni/com_apress_threads_MainActivity.h
+
+/Users/admin/Android_NDK/Chapter_7_Source_Code/app/src/main/jni/com_apress_threads_MainActivity.h:
